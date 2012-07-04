2495da7444e015360eddd6b72610c77c0772e0c6
aboutaaron <aaron.colby.williams@gmail.com>
2012-07-03 17:10:01 -0700

    added login info

--- photodb_scraper.rb
+++ photodb_scraper.rb
@@ -1,8 +1,14 @@
 require "rubygems"
 require "mechanize"
 
-agent = Mechanize.new
+agent = Mechanize.new do |a|
+    a.user_agent_alias = 'Mac Safari'
+end
 
 page = agent.get("http://laiac1b5z1-int.latimes.com/")
 
-form = page.forms.first
+form = page.forms[0]
+
+form["name"] = "jevon.phillips"
+form["password"] = "marvel"
+form.submit
