076b2afe935af88000336ef81c015edd39155546
aboutaaron <aaron.colby.williams@gmail.com>
2012-07-03 17:12:42 -0700

    part deux

--- photodb_scraper.rb
+++ photodb_scraper.rb
@@ -2,11 +2,12 @@ require "rubygems"
 require "mechanize"
 
 agent = Mechanize.new
-
 page = agent.get("http://laiac1b5z1-int.latimes.com/")
-
 form = page.forms[0]
 
+# Login
 form["name"] = "jevon.phillips"
 form["password"] = "marvel"
 form.submit
+
+# Next steps
