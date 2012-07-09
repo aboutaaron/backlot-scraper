78feb8392571b3fc6f486577ca7a62be7e487e45
aboutaaron <aaron.colby.williams@gmail.com>
2012-07-09 16:41:34 -0700

    adding a ton to refactor.rb

--- refactor.rb
+++ refactor.rb
@@ -1,15 +1,38 @@
 require "rubygems"
 require "mechanize"
 
+# Structure
+base_url = "http://laiac1b5z1-int.latimes.com"
+
 # Initialize Mechanize
 a = Mechanize.new
 
-a.get("http://laiac1b5z1-int.latimes.com/").forms.first do |x|
-    x.forms.first["name"] = "jevon.phillips"
-    x.forms.first["password"] = "marvel"
-    x.forms.first.submit
+# Login
+x = a.get("http://laiac1b5z1-int.latimes.com/").forms.first
+p "Submitting login info..."
+x["name"] = "jevon.phillips"
+x["password"] = "marvel"
+x.submit
 
-    # Click page
-    a.page.links[1].click
+# Click on Image page
+p "Opening up the 'Images' page..."
+a.page.links[1].click
 
-end
+# Click on first link
+a.page.link_with(:text => "Sarah Roemer plays catch with her dogs on the set of \"The Event\"").click
+
+# Possiblility for iteration:
+# a.page.links_with(:class => 'td:nth-child(3) a').each do |e| ....
+
+# next page CSS "#paging_content a"
+
+# Grab Descriptions, remove whitespace, store in variable photo_info and write to photo.txt
+photo_info = a.page.search("#image_description , #image_caption, #image_title").map(&:text).map(&:strip)
+p "Saving photo content to file..."
+File.open("photos.txt", 'w'){|file| file.write(photo_info)}
+
+
+
+# Open single photo page and save photo
+a.page.links[8].click
+
