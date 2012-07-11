fb699a7458f4b1dd7ec7173a9ac188c93cce5661
aboutaaron <aaron.colby.williams@gmail.com>
2012-07-10 18:21:16 -0700

    tightend up box office. Onward to google chart

--- boxoffice.rb
+++ boxoffice.rb
@@ -1,11 +1,12 @@
 require "rubygems"
 require "mechanize"
+require "google_chart"
 # require "pdf-reader"
 
 # Initialize new Mechanize class
 a = Mechanize.new
 
-# Set URI and user agent so Google doesn't redirect us
+# Set URI to redirct so Google doesn't redirect us to a non-functioning page
 uri = "https://accounts.google.com/ServiceLogin?service=mail&passive=true&rm=false&continue=http://mail.google.com/mail/&scc=1&ltmpl=default&ltmplcache=2"
 
 # Grab page, enter information and submit
@@ -15,18 +16,15 @@ x["Email"] = "boxoffice.lat@gmail.com"
 x["Passwd"] = "welcome to the times"
 x.submit
 
-# Google doesn't like the headless browser so it displays us a dummy page, which can be avoided by setting the basic HTML view as default.
-
-# We should now be at the main Email view. How about we print the first email to confirm?
-# In the basic view, the first email is usually the 25th indexed link item.
+# We should now be in the inbox. In the basic HTML view, the first email is usually the 25th indexed link item. So, let's find it and click it.
 p "Hopping in the first email..."
-a.page.links[25].click
+# a.page.links[25].click
+a.page.links[26].click
 
-# Setting the HTML view as variable for readability.
+
+# Now in the email body
+# Setting the HTML view as variable for readability when GET'ing it.
 html_view = a.page.link_with(:text=> "View as HTML").href
-
-# Uncomment below to click PDF instead
-# Coming soon...
 
 # Opening up the HTML view
 p "Opening HTML view of box office attachment..."
