8975428d6bcde58849dfb24aab2caa7016a5d47f
aboutaaron <aaron.colby.williams@gmail.com>
2012-07-24 18:10:19 -0700

    Able to shit descriptions and save file. Onto the last step

--- refactor.rb
+++ refactor.rb
@@ -17,6 +17,8 @@ x.submit
 
 # Click on Image page
 p "Opening up the 'Images' page with all images. This will take a while..."
+puts
+puts
 #a.page.links[1].click
 a.get("http://laiac1b5z1-int.latimes.com/images?content_partial=global%2Fcontent_list&filter_by=title&filter_status=all&filter_val=&limit=1000&offset=20&search_published=false&sort_asc=desc&sort_by=modified")
 
@@ -32,17 +34,12 @@ a.page.search("td:nth-child(3) a").each do |link|
     photo_info = a.page.search("#image_description , #image_caption, #image_title").map(&:text).map(&:strip)
     p photo_info
     puts
+    # Click largest image
+    a.page.link_with(:href => /\-sxga.jpg/).click
+
+    # Setting variables for download
+    img_url = a.page.search("img")[0].attributes['src'].text
+
+    # Download file
+    a.get(base_url+img_url).save
 end
-
-#File.open("photos.txt", 'w'){|file| file.write(photo_info)}
-
-# Open single photo page and save photo
-#a.page.links[8].click
-
-
-
-
-#uri = page.links[18].uri.to_s #gets the url
-#uri = uri[15..uri.index('&')-1]
-
-#agent.get(uri).save_as((File.basename(uri)).split("?")[0]) # saves the picture to script's directory
