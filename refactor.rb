ef3fef103709a34e5335f574394e78d85eff3597
aboutaaron <aaron.colby.williams@gmail.com>
2012-07-25 12:03:43 -0700

    added I/O operations and file work

--- refactor.rb
+++ refactor.rb
@@ -1,6 +1,14 @@
 require "rubygems"
 require "mechanize"
 require "open-uri"
+require "fileutils"
+
+# Files
+IMAGES = "images"
+Dir.mkdir(IMAGES) unless File.exists?(IMAGES)
+
+#CAPTIONS ="captions.txt"
+#File.open("captions.txt", "w"){ |captions| captions.puts "## Captions" } unless File.exists?(CAPTIONS)
 
 # Structure
 base_url = "http://laiac1b5z1-int.latimes.com"
@@ -19,7 +27,7 @@ x.submit
 p "Opening up the 'Images' page with all images. This will take a while..."
 puts
 puts
-#a.page.links[1].click
+# URL with maximum photo size
 a.get("http://laiac1b5z1-int.latimes.com/images?content_partial=global%2Fcontent_list&filter_by=title&filter_status=all&filter_val=&limit=1000&offset=20&search_published=false&sort_asc=desc&sort_by=modified")
 
 p "Iterating through links..."
@@ -32,14 +40,20 @@ a.page.search("td:nth-child(3) a").each do |link|
     a.get(image_page)
     # Grab Descriptions, remove whitespace, store in variable photo_info and write to photo.txt
     photo_info = a.page.search("#image_description , #image_caption, #image_title").map(&:text).map(&:strip)
-    p photo_info
-    puts
+    # Save info to text file.
+    CAPTIONS_FILE = File.open("captions.txt", "w")
+    CAPTIONS_FILE.puts "#{photo_info}"
+
+
     # Click largest image
     a.page.link_with(:href => /\-sxga.jpg/).click
 
     # Setting variables for download
     img_url = a.page.search("img")[0].attributes['src'].text
 
-    # Download file
+    # Change to Images Directory and download file
+    Dir.chdir("images/")
     a.get(base_url+img_url).save
 end
+
+CAPTIONS_FILE.close
